e221b3942e1b87ffa838b3c39335109232a2d5c5
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-04 23:05:17 -0400

    mvp

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const projectRouter = require('./routes/projectRoute');
+const actionRouter = require('./routes/actionRoute');
 
-Sing along:
+require('dotenv').config();
 
-here's a little code I wrote, you might want to read it really slow, don't worry be happy
-in every line there may be trouble, but if you worry you make it double, don't worry, be happy
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, be happy
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, be happy
+const port = process.env.PORT ? process.env.PORT : 4000;
+const server = express();
 
-Go code!
-*/
+server.use(cors());
+server.use(express.json());
+server.use(helmet());
+
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
+
+server.use(errorHandler);
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+function errorHandler(err, req, res, next) {
+  if (res.headerSent) {
+    return next(err);
+  }
+  res.status(500).json({
+    success: false,
+    err
+  });
+}
+
+module.exports = server;
